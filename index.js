e7b9fd0a90ee36d2db85cd11f24b4b72028c48a3
sajadadineh <sajadadineh@protonmail.com>
2020-05-29 02:28:12 +0430

    make commander option

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 #!/usr/bin/env node
 
 const { IgApiClient } = require("instagram-private-api");
+const { program } = require('commander');
+
+program
+  .requiredOption('-u, --username <type>', 'username on instagram')
+  .requiredOption('-p, --password <type>', 'user password')
+  .requiredOption('-m, --message <type>', 'send the message you want')
+  .requiredOption('-d, --desiredUsername <type>', 'the username you want to message')
+  .parse(process.argv);
 
 const ig = new IgApiClient();
 
-ig.state.generateDevice("username");
+ig.state.generateDevice(`${program.username}`);
 
 (async () => {
   await ig.simulate.preLoginFlow();
-  await ig.account.login("username", "password");
+  await ig.account.login(`${program.username}`, `${program.password}`);
   console.log("The user logged in")
-  const userId = await ig.user.getIdByUsername('desired username');
+  const userId = await ig.user.getIdByUsername(`${program.desiredUsername}`);
   const thread = ig.entity.directThread([userId.toString()]);
-  await thread.broadcastText('Message');
+  await thread.broadcastText(`${program.message}`);
   console.log("Message sent")
 })();

cc234c8f1a0aeda32523e4d09d9a6f7697e9829a
sajadadineh <sajadadineh@protonmail.com>
2020-06-01 16:58:22 +0430

    improve code

--- index.js
+++ index.js
@@ -25,58 +25,71 @@ program
     .command('get')
     .description('get data in db')
     .action(function(){
-        db.createReadStream({ reverse:true})
-            .on('data', function (data) {
-                console.log(chalk_blue(data.key.toString()) ,'=', chalk_green( data.value.toString()))
-            })
-            .on('error', function (err) {
-                console.log(err)
-            })
+        getDataInDB();
     })
 
 program
     .command('login')
     .description('login in instagram')
     .action(function(){
-        prompt.get(['username', 'password'], function (err, result) {
-            db.batch()
-                .put('username', result.username)
-                .put('password', result.password)
-                .write(function () { console.log(chalk_blue('processing ...'))})
-            Promise.all([
-                db.get('username'),
-                db.get('password')
-            ])
-            .then(arr => arr.map(element => element.toString())) // fix by iman ghvs
-            .then((arr)=>{
-                checkUsernameAndPassword(arr[0],arr[1])
-            })
-          })
+        loginInInstagram();
     })
 
 program
     .command('message')
     .description('message in direct')
     .action(function(){
-        prompt.get(['message', 'id'], function (err, result) {
-            db.batch()
-                .put('message', result.message)
-                .put('id', result.id)
-                .write(function () { console.log(chalk_db('save message and id in db'))})
-            Promise.all([
-                db.get("username"),
-                db.get("password"),
-                db.get("id"),
-                db.get("message"),
-            ])
-            .then(arr => arr.map(element => element.toString()))
-            .then((arr)=>{
-                processing(arr[0],arr[1],arr[2],arr[3])
-            })
-          })
+        directMessage();
     })
 
 program.parse(process.argv);
+
+
+function getDataInDB(){
+    db.createReadStream({ reverse:true})
+            .on('data', function (data) {
+                console.log(chalk_blue(data.key.toString()) ,'=', chalk_green( data.value.toString()))
+            })
+            .on('error', function (err) {
+                console.log(err)
+    })
+}
+
+function loginInInstagram(){
+    prompt.get(['username', 'password'], function (err, result) {
+        db.batch()
+            .put('username', result.username)
+            .put('password', result.password)
+            .write(function () { console.log(chalk_blue('processing ...'))})
+        Promise.all([
+            db.get('username'),
+            db.get('password')
+        ])
+        .then(arr => arr.map(element => element.toString())) // fix by iman ghvs
+        .then((arr)=>{
+            checkUsernameAndPassword(arr[0],arr[1])
+        })
+      })
+}
+
+function directMessage(){
+    prompt.get(['message', 'id'], function (err, result) {
+        db.batch()
+            .put('message', result.message)
+            .put('id', result.id)
+            .write(function () { console.log(chalk_db('save message and id in db'))})
+        Promise.all([
+            db.get("username"),
+            db.get("password"),
+            db.get("id"),
+            db.get("message"),
+        ])
+        .then(arr => arr.map(element => element.toString()))
+        .then((arr)=>{
+            processing(arr[0],arr[1],arr[2],arr[3])
+        })
+    })
+}
 
 async function checkUsernameAndPassword(username, password){
     ig.state.generateDevice(username);
